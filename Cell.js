89f6046187b6e91c5b52307eb09749e6fff57cc5
galit <galit1321@gmail.com>
2020-02-12 22:20:07 +0200

    commit project

---
+++ Cell.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Cell.css";
+
+class Cell extends Component {
+  render() {
+    const data = this.props.info;
+    if (data.full) {
+      return <div className={data.kind}> <h2 className="cell_id">
+        {data.id}
+      </h2></div>;
+    }
+    return <div className="emptyCell"></div>;
+  }
+}
+export default Cell;
